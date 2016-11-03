5954c309e3f3c723a6edee5e1bf72a2e95dc570f
Prajna Kandarpa <prajna.kandarpa@uwaterloo.ca>
2016-11-03 14:45:56 -0400

    remove sudo as the docker group has appropriate permissions now

--- run.sh
+++ run.sh
@@ -14,11 +14,11 @@ popd > /dev/null
 set -e
 
 # Run the container with shared X11
-sudo docker run\
+docker run\
   --net=host\
   -e SHELL\
   -e DISPLAY\
   -e DOCKER=1\
   -v "$HOME:$HOME:rw"\
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
-  -it $1 $SHELL
+  -it $1 /bin/bash
